807ee485c9bac746fe05883ff8fc5e08052f2c48
Katherine <Katherine@DESKTOP-OOPNAJB>
2026-05-17 22:23:40 -0600

    Terminado While.java - Katherine Lúe

---
+++ .idea/src/While.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args)
+    {
+        int i = 1;
+        System.out.println("Primero 1000 números naturales");
+
+        while(i <= 1000)
+        {
+            System.out.println(i);
+            i+=1;
+        }
+    }
+
+}
